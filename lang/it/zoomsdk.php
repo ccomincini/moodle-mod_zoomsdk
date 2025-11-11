bfcf0a0117f5e8d688272259a38cb45d6c8e1caf
Carlo Comincini <carlo@comincini.it>
2025-11-11 23:37:21 +0100

    Ripristino stringhe settings: sdkkey, sdkkey_desc, sdksecret, sdksecret_desc (it)

--- lang/it/zoomsdk.php
+++ lang/it/zoomsdk.php
@@ -1,42 +1,8 @@
 <?php
-// Stringhe italiane aggiornate (Sicurezza, Media, Ricorrenza avanzata)
-
-defined('MOODLE_INTERNAL') || die();
-$string['pluginname'] = 'Zoom SDK Meeting';
-$string['modulename'] = 'Riunione Zoom SDK';
-$string['general'] = 'Generale';
-$string['security'] = 'Sicurezza';
-$string['media'] = 'Media';
-$string['breakoutrooms'] = 'Breakout rooms';
-$string['meetingname'] = 'Nome riunione';
-$string['starttime'] = 'Ora di inizio';
-$string['duration'] = 'Durata';
-$string['meetingid'] = 'ID riunione';
-$string['requirepasscode'] = 'Password riunione obbligatoria';
-$string['meetingpassword'] = 'Imposta password';
-$string['passwordhelp'] = 'La password può contenere solo lettere, numeri, @ _ * - . e deve essere lunga 1-10 caratteri.';
-$string['option_waiting_room'] = 'Abilita sala d’attesa';
-$string['option_jbh'] = 'Consenti di partecipare alla riunione in qualsiasi momento';
-$string['option_authenticated_users'] = 'Richiedi autenticazione per partecipare';
-$string['option_host_video'] = 'Video host';
-$string['option_participants_video'] = 'Video partecipanti';
-$string['option_audio'] = 'Opzioni audio';
-$string['option_mute_upon_entry'] = 'Microfono dei partecipanti muto all’accesso';
-$string['option_auto_recording'] = 'Registrazione automatica';
-$string['recurring'] = 'Meeting ricorrente';
-$string['recurrence_type'] = 'Tipo di ricorrenza';
-$string['repeat_interval'] = 'Intervallo di ripetizione';
-$string['weekly_days_group'] = 'Ripeti nei giorni';
-$string['monthly_day'] = 'Giorno del mese';
-$string['end_date_option'] = 'Termina ricorrenza';
-$string['end_date_time'] = 'Termina entro il';
-$string['end_times'] = 'Termina dopo occorrenze';
-$string['end_times_text'] = 'occorrenze';
-$string['breakoutrooms_info'] = 'Nessuna stanza<br>Aggiungi una room cliccando';
-// Errori
-$string['password_help'] = 'Password obbligatoria, 1-10 caratteri validi.';
-$string['err_invalid_password'] = 'Formato password non valido.';
-$string['err_password_required'] = 'Password obbligatoria';
-// View e meeting
-$string['joinmeeting'] = 'Partecipa alla Riunione (Integrata)';
-$string['connecting'] = 'Connessione in corso...';
+// Stringhe italiane Zoom SDK Meeting (completo).
+// ... (altre stringhe già presenti) ...
+$string['sdkkey'] = 'SDK Key';
+$string['sdkkey_desc'] = 'La tua Zoom Meeting SDK Key da marketplace.zoom.us';
+$string['sdksecret'] = 'SDK Secret';
+$string['sdksecret_desc'] = 'Il tuo Zoom Meeting SDK Secret (mantienilo riservato!)';
+// ... (altre stringhe già presenti) ...
