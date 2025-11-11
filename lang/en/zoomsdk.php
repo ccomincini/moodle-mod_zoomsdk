cce2a8eeeeec697f86ebc77fcea0658f21eb35d4
Carlo Comincini <carlo@comincini.it>
2025-11-11 22:35:18 +0100

    Add recurring meeting strings (English)

--- lang/en/zoomsdk.php
+++ lang/en/zoomsdk.php
@@ -38,9 +38,55 @@ $string['starttime'] = 'Start time';
 $string['duration'] = 'Duration';
 $string['meetingid'] = 'Meeting ID';
 
+// Meeting type.
+$string['meetingtype'] = 'Meeting type';
+$string['meetingtype_help'] = 'Choose the type of meeting to create';
+$string['type_instant'] = 'Instant meeting';
+$string['type_scheduled'] = 'Scheduled meeting';
+$string['type_recurring_fixed'] = 'Recurring meeting with fixed time';
+$string['type_recurring_nofixed'] = 'Recurring meeting without fixed time';
+
+// Recurrence settings.
+$string['recurrencesettings'] = 'Recurrence settings';
+$string['recurrence_type'] = 'Recurrence type';
+$string['recurrence_type_help'] = 'How often the meeting repeats';
+$string['recurrence_daily'] = 'Daily';
+$string['recurrence_weekly'] = 'Weekly';
+$string['recurrence_monthly'] = 'Monthly';
+$string['repeat_interval'] = 'Repeat every';
+$string['repeat_interval_help'] = 'Number of days/weeks/months between occurrences';
+$string['weekly_days'] = 'Repeat on';
+$string['weekly_days_help'] = 'Select the days of the week for the meeting';
+$string['day_sunday'] = 'Sunday';
+$string['day_monday'] = 'Monday';
+$string['day_tuesday'] = 'Tuesday';
+$string['day_wednesday'] = 'Wednesday';
+$string['day_thursday'] = 'Thursday';
+$string['day_friday'] = 'Friday';
+$string['day_saturday'] = 'Saturday';
+$string['monthly_day'] = 'Day of month';
+$string['monthly_day_help'] = 'Day of the month (1-31)';
+$string['monthly_week'] = 'Week of month';
+$string['monthly_week_help'] = 'Which week of the month';
+$string['monthly_week_day'] = 'Day of week';
+$string['monthly_week_day_help'] = 'Which day of the week';
+$string['week_last'] = 'Last';
+$string['week_first'] = 'First';
+$string['week_second'] = 'Second';
+$string['week_third'] = 'Third';
+$string['week_fourth'] = 'Fourth';
+$string['end_times'] = 'Number of occurrences';
+$string['end_times_help'] = 'How many times the meeting will occur (max 60)';
+$string['end_date_time'] = 'End by date';
+$string['end_date_time_help'] = 'Last date for recurring meetings';
+
 // Validation errors.
 $string['starttime_past'] = 'Start time cannot be in the past';
 $string['duration_toolow'] = 'Duration must be at least 1 minute';
+$string['err_weekly_days'] = 'You must select at least one day for weekly recurrence';
+$string['err_end_times'] = 'Number of occurrences must be between 1 and 60';
+$string['err_repeat_interval'] = 'Repeat interval must be at least 1';
+$string['err_monthly_day'] = 'Day of month must be between 1 and 31';
 
 // View.
 $string['joinmeeting'] = 'Join Meeting (Embedded)';
@@ -78,5 +124,4 @@ $string['zoomsdk:viewattendance'] = 'View meeting attendance';
 
 // Scheduled tasks.
 $string['task_fetch_attendance'] = 'Fetch Zoom attendance records';
-
 $string['task_cleanup_deleted'] = 'Clean up deleted Zoom SDK modules';
