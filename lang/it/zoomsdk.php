897ba443f3203f8dae597911dae3462937383c3c
Carlo Comincini <carlo@comincini.it>
2025-11-11 22:35:56 +0100

    Add recurring meeting strings (Italian)

--- lang/it/zoomsdk.php
+++ lang/it/zoomsdk.php
@@ -38,9 +38,55 @@ $string['starttime'] = 'Ora di inizio';
 $string['duration'] = 'Durata';
 $string['meetingid'] = 'ID riunione';
 
+// Meeting type.
+$string['meetingtype'] = 'Tipo di riunione';
+$string['meetingtype_help'] = 'Scegli il tipo di riunione da creare';
+$string['type_instant'] = 'Riunione immediata';
+$string['type_scheduled'] = 'Riunione programmata';
+$string['type_recurring_fixed'] = 'Riunione ricorrente con orario fisso';
+$string['type_recurring_nofixed'] = 'Riunione ricorrente senza orario fisso';
+
+// Recurrence settings.
+$string['recurrencesettings'] = 'Impostazioni ricorrenza';
+$string['recurrence_type'] = 'Tipo di ricorrenza';
+$string['recurrence_type_help'] = 'Con quale frequenza si ripete la riunione';
+$string['recurrence_daily'] = 'Giornaliera';
+$string['recurrence_weekly'] = 'Settimanale';
+$string['recurrence_monthly'] = 'Mensile';
+$string['repeat_interval'] = 'Ripeti ogni';
+$string['repeat_interval_help'] = 'Numero di giorni/settimane/mesi tra le occorrenze';
+$string['weekly_days'] = 'Ripeti il';
+$string['weekly_days_help'] = 'Seleziona i giorni della settimana per la riunione';
+$string['day_sunday'] = 'Domenica';
+$string['day_monday'] = 'Lunedì';
+$string['day_tuesday'] = 'Martedì';
+$string['day_wednesday'] = 'Mercoledì';
+$string['day_thursday'] = 'Giovedì';
+$string['day_friday'] = 'Venerdì';
+$string['day_saturday'] = 'Sabato';
+$string['monthly_day'] = 'Giorno del mese';
+$string['monthly_day_help'] = 'Giorno del mese (1-31)';
+$string['monthly_week'] = 'Settimana del mese';
+$string['monthly_week_help'] = 'Quale settimana del mese';
+$string['monthly_week_day'] = 'Giorno della settimana';
+$string['monthly_week_day_help'] = 'Quale giorno della settimana';
+$string['week_last'] = 'Ultima';
+$string['week_first'] = 'Prima';
+$string['week_second'] = 'Seconda';
+$string['week_third'] = 'Terza';
+$string['week_fourth'] = 'Quarta';
+$string['end_times'] = 'Numero di occorrenze';
+$string['end_times_help'] = 'Quante volte si ripeterà la riunione (max 60)';
+$string['end_date_time'] = 'Termina entro il';
+$string['end_date_time_help'] = 'Data finale per le riunioni ricorrenti';
+
 // Errori di validazione.
 $string['starttime_past'] = 'L\'ora di inizio non può essere nel passato';
 $string['duration_toolow'] = 'La durata deve essere di almeno 1 minuto';
+$string['err_weekly_days'] = 'Devi selezionare almeno un giorno per la ricorrenza settimanale';
+$string['err_end_times'] = 'Il numero di occorrenze deve essere compreso tra 1 e 60';
+$string['err_repeat_interval'] = 'L\'intervallo di ripetizione deve essere almeno 1';
+$string['err_monthly_day'] = 'Il giorno del mese deve essere compreso tra 1 e 31';
 
 // View.
 $string['joinmeeting'] = 'Partecipa alla Riunione (Integrata)';
@@ -77,4 +123,5 @@ $string['zoomsdk:view'] = 'Visualizzare la riunione Zoom SDK';
 $string['zoomsdk:viewattendance'] = 'Visualizzare le presenze alla riunione';
 
 // Scheduled tasks.
-$string['task_fetch_attendance'] = 'Recupera presenze da Zoom';$string['task_cleanup_deleted'] = 'Pulisci moduli Zoom SDK eliminati';
+$string['task_fetch_attendance'] = 'Recupera presenze da Zoom';
+$string['task_cleanup_deleted'] = 'Pulisci moduli Zoom SDK eliminati';
